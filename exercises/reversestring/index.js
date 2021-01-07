f511a0886ec0f7a690f932684e5a3ece1df19f8c
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-01-07 21:45:39 +0100

    Algorithm exercises

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+//3rd Solution
+function reverse(str) {
+    return str.split("").reduce((rev,char) => char + rev,"")
+}
+
+module.exports = reverse;
+
+//1st solution
+// function reverse(str) {
+//     return str.split("").reverse().join("");
+    
+// }
+
+//2nd solution
+// function reverse(str) {
+//     let reversed = "";
+//     for (let character in str){
+//         reversed = character + reversed;
+//     }
+//     return reversed
+// }
