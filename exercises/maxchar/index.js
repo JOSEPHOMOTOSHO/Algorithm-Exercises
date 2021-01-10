d26818fd1d13b5ed8652be3ddc4fe208e08fe054
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-01-11 00:31:58 +0100

    Max char Algorithm

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,43 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {};
+    max = 0;
+    charmax = ""
+  for(let char of str){
+  chars[char] = chars[char] + 1 || 1; //LOGICAL OR RETURNS A TRUTHY VALUE
+  //CHARS[CHAR] VALUE IS NOT ASSIGNED IT IS FALSY SO 1(TRUTHY) IS ASSIGNED TO CHARS[CHAR]
+  }
+  for(let char in chars){
+    if(chars[char] > max){
+      max = chars[char];
+      charmax = char
+    }
+  }
+  return charmax
+  }
 
+  
+//Longer solution with if statements
+
+//   function maxChar(str) {
+//     let chars = {};
+//     max = 0;
+//     charmax = ""
+//   for(let char of str){
+//   if(chars[char]){
+//     chars[char]++
+//   }else{
+//     chars[char] = 1;
+//   }
+//   }
+//   for(let char in chars){
+//     if(chars[char] > max){
+//       max = chars[char];
+//       charmax = char
+//     }
+//   }
+//   return charmax
+//   }
 module.exports = maxChar;
