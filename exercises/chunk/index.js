3de5e1f59c4af7f66adec2b893edfe50cb0a0dfa
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-02-09 18:33:01 +0100

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunk = [];
+    let start = 0;
+    while(start < array.length){
+      chunk.push(array.slice(start, start+size))
+      start += size 
+    }
+    return chunk
+    }
+
+//For loop method
+// function chunk(array, size) {
+//     let chunk = [];
+//     for (let element of array){
+//         let last = chunk[chunk.length - 1];
+//         if(!last || last.length == size){
+//             chunk.push([element])
+//         }else{
+//             last.push(element)
+//         }
+//     }
+//     return chunk
+    
+//     }
 
 module.exports = chunk;
